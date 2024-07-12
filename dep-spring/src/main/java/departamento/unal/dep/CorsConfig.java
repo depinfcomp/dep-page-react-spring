3dc13079b42f3b60b08b723c86ef9d38b70b4e40
Julian Lopez Marulanda <jullopezm@unal.edu.co>
2024-07-12 15:13:06 -0500

    modlocal

--- dep-spring/src/main/java/departamento/unal/dep/CorsConfig.java
+++ dep-spring/src/main/java/departamento/unal/dep/CorsConfig.java
@@ -10,12 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://192.168.1.1:5173")
-                .allowedOrigins("http://172.22.80.1:5173")
-                .allowedOrigins("http://192.168.40.57:80")
-                .allowedOrigins("http://192.168.4.34:80")
+                .allowedOrigins("http://192.168.1.24")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
+
+
