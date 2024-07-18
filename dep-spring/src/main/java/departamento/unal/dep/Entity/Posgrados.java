11030d66097aadccd51b4e2f4700145cdbcade62
Julian Lopez Marulanda <jullopezm@unal.edu.co>
2024-07-18 15:38:42 -0500

    up

---
+++ dep-spring/src/main/java/departamento/unal/dep/Entity/Posgrados.java
@@ -0,0 +1,39 @@
+package departamento.unal.dep.Entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Posgrados", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "titulo")
+})
+public class Posgrados {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false, unique = true)
+    private String titulo;
+
+    @Column(name = "Fecha_Inscripcion", nullable = false)
+    private Date fechaInscripcion;
+
+    @Column(name = "Fecha_Inscripcion", nullable = false)
+    private Date fechaCierre;
+
+    @Column(name = "Contacto", nullable = false, length = 255)
+    private String contacto;
+
+    @Column(name = "Link_Informacion", nullable = false, length = 255)
+    private String linkInformacion;
+
+    @Column(name = "Link_inscripcion", nullable = false, length = 255)
+    private String linkInscripcion;
+}
